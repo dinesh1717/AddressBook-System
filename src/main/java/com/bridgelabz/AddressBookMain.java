c387c43e89ca3813bdf5563ae996b4ba26d3f998
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-25 00:13:13 +0530

    added multiple contacts to address book

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,18 +1,40 @@
 package com.bridgelabz;
 import java.util.Scanner;
 public class AddressBookMain {
+    /**
+     * Main method for manipulation AddressBookCollection
+     *
+     */
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
         Scanner scanner = new Scanner(System.in);
+        /**
+         * while condition is taken to check the condition is it is true enters to the
+         * switch case statement
+         */
+        while (true) {
+            System.out.println(
+                    "Enter \n 1 To add The contact \n 2 To edit the contact \n 3 To delete the contact \n 4 to exit");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addressBook.addContact();
+                    break;
+                case 2:
+                    addressBook.editContact();
+                    break;
+                case 3:
+                    addressBook.deleteContact();
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Enter the wrong input");
+                    continue;
+            }
 
-        addressBook.addContact();
-        System.out.println("Enter 1 To Edit The Contact and 2 To Delete The Contact ");
-        int choice = scanner.nextInt();
-        if (choice == 1) {
-            addressBook.editContact();
-        } else {
-            addressBook.deleteContact();
         }
+
     }
 }
-
